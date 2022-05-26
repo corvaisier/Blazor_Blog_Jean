9e6955bd2f23539ea0b4004ce1c8e2e444a08481
corvaisier <julien.corvaisier.br@gmail.com>
2022-05-26 20:49:41 +0200

    add Web new article form and dedicated page

---
+++ Server/Migrations/20220526152059_CleanPostModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor_Blog_Jean.Server.Migrations
+{
+    public partial class CleanPostModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "BlogPosts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "BlogPosts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "BlogPosts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "BlogPosts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- Shared/Article.cs
+++ Shared/Article.cs
@@ -7,9 +7,6 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public string Content { get; set; }
-        public string Author { get; set; }
         public DateTime DateCreated { get; set; }
-        public bool IsPublished { get; set; } = true;
-        public bool IsDeleted { get; set; } = false;
     }
 }
