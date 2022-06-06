48a5b00e463e3494d175ef0dc898da688b74aba9
corvaisier <julien.corvaisier.br@gmail.com>
2022-06-06 17:55:42 +0200

    add authentication need to create new articles

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,4 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IBlogService, BlogService>();
 
+//add for authentication
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
