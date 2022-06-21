de74e08deb4029042cc7de2998ff5074009ca705
corvaisier <julien.corvaisier.br@gmail.com>
2022-06-21 23:52:38 +0200

    add delete and update system, need to manage errors

--- Server/Controllers/BlogController.cs
+++ Server/Controllers/BlogController.cs
@@ -25,11 +25,29 @@ namespace Blazor_Blog_Jean.Server.Controllers
         public ActionResult<Article> GetSingleBlogPostByUrl(string url)
         {
             var post = _dataContext.BlogPosts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
-            if (post == null) 
+            if (post == null)
             {
                 return NotFound("Ce post n'existe pas.");
             };
             return Ok(post);
+        }
+
+        [HttpDelete("{url}")]
+        public ActionResult<Article> DeleteBlogPostByUrl(string url)
+        {
+            Article post = _dataContext.BlogPosts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
+
+            if (post == null)
+            {
+                return NotFound("Ce post n'existe pas.");
+            } else
+            {
+                _dataContext.Remove(post);
+                _dataContext.SaveChanges();
+                Console.WriteLine("controller ok!");
+                return Ok(post);
+            }
+            
         }
 
         [HttpPost]
@@ -39,6 +57,16 @@ namespace Blazor_Blog_Jean.Server.Controllers
             await _dataContext.SaveChangesAsync();
 
             return request;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Article>> UpdateBlogPost(Article request)
+        {
+                _dataContext.Update(request);
+                await _dataContext.SaveChangesAsync();
+
+                return Ok(request);
+
         }
     }
 }
