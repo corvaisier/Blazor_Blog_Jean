df38d33c53e1fbbd466fc388aca3f33d3827779b
corvaisier <julien.corvaisier.br@gmail.com>
2022-06-25 15:40:51 +0200

    add error handling in controller

--- Server/Controllers/BlogController.cs
+++ Server/Controllers/BlogController.cs
@@ -18,13 +18,23 @@ namespace Blazor_Blog_Jean.Server.Controllers
         [HttpGet]
         public ActionResult<List<Article>> GetAllBlogPosts()
         {
-            return Ok(_dataContext.BlogPosts);
+            var posts = _dataContext.BlogPosts;
+
+            if (posts == null)
+            {
+                return NotFound("Ces post ne sont pas accessibles pour le moment.");
+            }
+            else
+            {
+                return Ok(_dataContext.BlogPosts);
+            }
         }
 
         [HttpGet("{url}")]
         public ActionResult<Article> GetSingleBlogPostByUrl(string url)
         {
             var post = _dataContext.BlogPosts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
+
             if (post == null)
             {
                 return NotFound("Ce post n'existe pas.");
@@ -35,7 +45,7 @@ namespace Blazor_Blog_Jean.Server.Controllers
         [HttpDelete("{url}")]
         public ActionResult<Article> DeleteBlogPostByUrl(string url)
         {
-            Article post = _dataContext.BlogPosts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
+            var post = _dataContext.BlogPosts.FirstOrDefault(p => p.Url.ToLower().Equals(url.ToLower()));
 
             if (post == null)
             {
@@ -46,27 +56,38 @@ namespace Blazor_Blog_Jean.Server.Controllers
                 _dataContext.SaveChanges();
                 Console.WriteLine("controller ok!");
                 return Ok(post);
-            }
-            
+            }          
         }
 
         [HttpPost]
         public async Task<ActionResult<Article>> CreateNewBlogPost(Article request)
         {
-            _dataContext.Add(request);
-            await _dataContext.SaveChangesAsync();
+            try
+            {
+                _dataContext.Add(request);
+                await _dataContext.SaveChangesAsync();
 
-            return request;
+                return request;
+
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpPut]
         public async Task<ActionResult<Article>> UpdateBlogPost(Article request)
         {
+            try {
                 _dataContext.Update(request);
                 await _dataContext.SaveChangesAsync();
 
                 return Ok(request);
 
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
